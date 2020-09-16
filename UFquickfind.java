324ee27fe972db0a8608a72be95dc528182b3138
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2020-09-16 11:52:17 -0400

    implemented quick find algo

--- UFquickfind.java
+++ UFquickfind.java
@@ -1,11 +1,13 @@
-/* Algorithm 1.5 Union-Find implementation */
+/* Algorithm 1.5 Union-Find implementation using quick find
+a naive slow approach but good for illustrating the power of
+efficient implentations */
 
-public class UF 
+public class UFquickfind 
 {
     private int[] id; // access to component id (site indexed)
     private int count; // number of components
 
-    public UF(int N)
+    public UFquickfind(int N)
     {  // Initialize component id array
         count = N;
         id = new int[N];
@@ -21,11 +23,29 @@ public class UF
     // Todo
     // public int find(int p)
     // public void union(int p, int q)
+    // Quick find 
+    public int find(int p)
+    {   return id[p];    }
+
+    public void union(int p, int q)
+    {
+        // Put p and q into the same component
+        int pID = find(p);
+        int qID = find(q);
+
+        // Nothing to do if p and q are already in the same component
+        if (pID == qID) return;
+
+        // Rename p's component to q's name
+        for (int i = 0; i < id.length; i++)
+            if (id[i] == pID) id[i] = qID;
+        count--;
+    }
 
     public static void main(String[] args)
     { // Solve dynamic connectivity problem on StdIn 
         int N = StdIn.readInt();    // Read number of sites
-        UF uf = new UF(N);  // Initialize N components
+        UFquickfind uf = new UFquickfind(N);  // Initialize N components
         while (!StdIn.isEmpty())
         {
             int p = StdIn.readInt();
