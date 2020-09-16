becdc4752f84d810f9c32a399c20fca7e21aea76
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2020-09-16 12:59:07 -0400

    implemented weighted union

--- WeightedQuickUnionUF.java
+++ WeightedQuickUnionUF.java
@@ -15,4 +15,29 @@ public class WeightedQuickUnionUF {
         sz = new int[N];
         for (int i = 0; i < N; i++) sz[i] = i;
     }
+
+    public int count()
+    {   return count;   }
+
+    public boolean connected(int p, int q)
+    {   return find(p) == find(q);  }
+
+    private int find(int p)
+    {
+        // Follow links to find a root
+        while (p != id[p]) p = id[p];
+        return p;
+    }
+
+    public void union(int p, int q)
+    {
+        int i = find(p);
+        int j = find(q);
+        if (i == j) return;
+
+        // Make smaller root point to larger one
+        if (sz[i] < sz[j]) { id[i] = j; sz[j] += sz[i]; }
+        else    { id[j] = i; sz[i] += sz[j]; }
+        count--;
+    }
 }
