faea25fb6ce49d3da1fb12c9526af258802425b4
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2020-09-23 09:59:01 -0400

    implemented partition

--- Quick.java
+++ Quick.java
@@ -17,7 +17,24 @@ public class Quick
 
     }
 
-    // Todo
-    // Partition
+    private static int partition(Comparable[] a, int lo, int hi)
+    {
+        // Partition into a[lo..i-1], a[i], a[i+1..hi]
+        int i = lo, j = hi + 1; // left and right scan indices
+        Comparable v = a[lo]; // partitioning item
+        while (true)
+        {
+            // Scan right, scan left, check for scan complete, and exchange
+            while (less(a[++i], v)) if (i == hi) break;
+            while (less(v, a[--j])) if (j == lo) break;
+            if (i >= j) break;
+            exch(a, i, j);
+        }
+        exch(a, lo, j); // Put v = a[j] into position
+        return j; // with a[lo..j-1] <= a[j] <= a[j+1..hi]
+    }
+
+    // todo
+    // exch()
     
 }
