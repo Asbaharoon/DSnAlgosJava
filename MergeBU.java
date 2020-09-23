ae81e22cbf81e60f1ed879ede9801b206f76ebe8
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2020-09-23 08:27:48 -0400

    implemented Bottom-up mergesort

---
+++ MergeBU.java
@@ -0,0 +1,20 @@
+// Bottom-up mergesort
+
+public class MergeBU
+{
+    private static Comparable[] aux; // auxiliary array for merges
+
+    public static void sort(Comparable[] a)
+    {
+        // Do lg N passes of pairwise merges.
+        int N = a.length;
+        aux = new Comparable[N];
+        for (int sz = 1; sz < N; sz = sz+sz) // sz: subarray size
+            for (int lo = 0; lo < N - sz; lo += sz + sz) // lo: subarray index
+                merge(a, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, N - 1));
+    }
+
+    // Todo
+    // merge()
+    
+}
