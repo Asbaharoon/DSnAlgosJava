1755e561f8bd778e4319d983f83ca062d9037d53
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2020-10-27 11:17:49 -0400

    implemented Dijkstra shortest path algo

---
+++ DijkstraSP.java
@@ -0,0 +1,41 @@
+// Algorithm 4.9 Dijkstra's shortest paths algorithm
+
+public class DijkstraSP {
+    private DirectedEdge[] edgeTo;
+    private double[] distTo;
+    private IndexMinPQ<Double> pq;
+
+    public DijkstaSP(EdgeWeightedDigraph G, int s)
+    {
+        edgeTo = new DirectedEdge[G.V()];
+        distTo = new double[G.V()];
+        pq = new IndexMinPQ<Double>(G.V());
+
+        for (int v = 0; v < G.V(); v++)
+            distTo[v] = Double.POSITIVE_INFINITY;
+        distTo[s] = 0.0;
+
+        pq.inserts(s, 0.0);
+        while(!pq.isEmpty())
+            relax(G, pq.delMin())
+    }
+
+    private void relax(EdgeWeightedDigraph G, int v)
+    {
+        for (DirectedEdge e : G.adj(v))
+        {
+            int w = e.to();
+            if (distTo[w] > distTo[v] + e.weight())
+            {
+                distTo[w] = distTo[v] + e.weight();
+                edgeTo[w] = e;
+                if (pq.contains(w)) pq.change(w, distTo[w]);
+                else pq.insert(w, distTo[w]);
+            }
+        }
+    }
+
+    public double distTo(int v)
+    public boolean hasPathTo(int v)
+    public Iterable<Edge> pathTo(int v)
+}
