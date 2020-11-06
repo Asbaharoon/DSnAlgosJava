1fc7c88bf091155187318657052c0bf6756e0dd8
Ogah Jackreece Ejini <jackreeceejini@gmail.com>
2020-11-06 10:52:48 -0500

    implemented keysThatMatch, collect (overloaded)

--- TrieST.java
+++ TrieST.java
@@ -57,4 +57,24 @@ public class TrieST<Value>
         for (char c = 0; c < R; c++)
             collect(x.next[c], pre + c, q);
     }
+
+    public Iterable<String> keysThatMatch(String pat)
+    {
+        Queue<String> q = new Queue<String>();
+        collect(root, "", pat, q);
+        return q;
+    }
+
+    public void collect(Node x, String pre, String pat, Queue<String> q)
+    {
+        int d = pre.length();
+        if (x == null) return;
+        if (d == pat.length() && x.val != null) q.enqueue(pre);
+        if (d == pat.length()) return;
+
+        char next = pat.charAt(d);
+        for (char c = 0; c < R; c++)
+            if (next == '.' || next == c)
+                collect(x.next[c], pre + c, pat, q);
+    }
 }
